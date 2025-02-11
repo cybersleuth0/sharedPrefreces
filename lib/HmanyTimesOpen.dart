77e72497a61f21793e4f472c9eca5ce20a4d0bec
cybersleuth0 <ayushshende83@gmail.com>
2025-02-11 14:30:01 +0530

    added how many times we open the app using sharedPrefrences

---
+++ lib/HmanyTimesOpen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class hmanyTimesOpen extends StatefulWidget {
+  @override
+  State<hmanyTimesOpen> createState() => hmanyTimesOpenState();
+}
+
+class hmanyTimesOpenState extends State<hmanyTimesOpen> {
+  int howmanytimes = 0; //use for show the times you open the app
+  String PREF_COUNT_KEY = "count";
+
+  initState() {
+    super.initState();
+    showtimes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text.rich(
+          TextSpan(text: "You Open This app ", children: [
+            TextSpan(
+                text: "$howmanytimes",
+                style: const TextStyle(color: Colors.red, fontSize: 35)),
+            const TextSpan(text: " times")
+          ]),
+          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  void showtimes() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    howmanytimes = pref.getInt(PREF_COUNT_KEY) ?? 0;
+    howmanytimes++;
+    pref.setInt(PREF_COUNT_KEY, howmanytimes);
+    setState(() {});
+  }
+}
